2f77e5bbdf3b99b1baee1783addc8d6ac71eac76
Tuvshee555 <tuvshee894@gmail.com>
2025-02-21 18:49:51 +0800

    sss

---
+++ src/routers/user-router.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use("/", (req, res) => {
+    // const {as}
+    res.send("This is the best")
+})
+
+app.listen(port, () => {
+    console.log(`example app listening on port ${port}`);
+    
+})
